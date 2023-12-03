f02517828b32c755cf768dc92d90bd494506c875
GiftGiwa <gdg2131@columbia.edu>
2023-12-02 19:39:06 -0500

    add GLAD

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CC      = g++
+CPP		= g++
 # CC		= clang++
 TARGET  = raytracer
 C_FILES = $(filter-out $(TARGET).cc, $(wildcard *.cc))
@@ -10,18 +10,23 @@ SRCS    = $(shell cat sourcefiles.txt)
 BUILD_DIR = build/src
 
 OBJS = $(patsubst $(SRC_DIR)/%.cc, $(BUILD_DIR)/%.o, $(SRCS))
-
+OBJS += $(BUILD_DIR)/glad.o
 CFLAGS = -g -Wall -Werror -pedantic-errors -Wno-c++11-extensions
-LDFLAGS =
+IFLAGS = -Iglad/include
+LDFLAGS = -lglfw
 
 .PHONY: all clean
 all: $(TARGET)
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cc | $(BUILD_DIR)
-	$(CC) -std=c++11 $(CFLAGS) -c $< -o $@
+	$(CPP) -std=c++11 $(CFLAGS) $(IFLAGS) -c $< -o $@
+
+
+$(BUILD_DIR)/glad.o: glad/glad.c | $(BUILD_DIR)
+	$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
 
 $(TARGET): $(OBJS)
-	$(CC) -std=c++11 $(CFLAGS) $^ -o $@ $(LDFLAGS) -lglfw
+	$(CPP) -std=c++11 $(CFLAGS) $^ -o $@ $(LDFLAGS)
 
 clean:
 	find $(BUILD_DIR) -name '*.o' -type f -delete
--- src/main/main.cc
+++ src/main/main.cc
@@ -6,24 +6,120 @@
 #include "../renderer/renderer.h"
 
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width
+    // and height will be significantly larger than specified on retina displays
+    glViewport(0, 0, width, height);
+}
 
 
 int main() { 
 
     // GLFWwindow* window;
-
+/*
     if (!glfwInit()) {
+        std::cout << "Error: could not initialize GLFW.\n";
+        return 1;
+    }
+
+    std::cout << "initialization success\n";
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
+    if (!window) {
+        std::cout << "Error: window or OpenGL context creation failed\n";
+        return 1;
+    }
+
+    std::cout << "window creation success\n";
+
+    glfwMakeContextCurrent(window);
+    // gladLoadGL(glfwGetProcAddress);
+    // glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    std::cout << "success!\n";
-    // GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-    // if (!window) {
-    //     std::cout << "Error: window or OpenGL context creation failed\n";
-    //     return -1;
-    // }
 
-    
+    while (!glfwWindowShouldClose(window))
+    {
+        // Keep running
+        // input
+        // processInput(window);
+
+        // rendering commands here
+        // ...
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap the buffers
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+
+    }
 
 
+    glfwDestroyWindow(window);
+ 
+    glfwTerminate();
+
+*/
+
+
+    GLFWwindow* window;
+
+    // Initialize the library
+    if(!glfwInit())
+        return -1;
+
+    // Define version and compatibility settings
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Mathe the window's context current
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Initialize the OpenGL API with GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Loop until the user closes the window
+    while(!glfwWindowShouldClose(window))
+    {
+        // Render here!
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
+
+
+//     return EXIT_SUCCESS;
+// }
